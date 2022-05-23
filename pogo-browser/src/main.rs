c51ac251137e763ff7b8563b0b48d4bb37929b1b
Marti Raudsepp <marti@juffo.org>
2022-05-23 04:51:31 +0300

    Initial attempt at using Yew (WIP!)

--- pogo-browser/src/main.rs
+++ pogo-browser/src/main.rs
@@ -1,3 +1,47 @@
+use yew::prelude::*;
+use yew_bootstrap::component::Button;
+use yew_bootstrap::util::{include_inline, Color};
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                {include_inline()}
+                <Button style={Color::Primary} onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</Button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }

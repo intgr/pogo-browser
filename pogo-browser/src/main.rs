ae9f9d5a78fc7dc60cfc133a1892b616c8589fdd
Marti Raudsepp <marti@juffo.org>
2022-05-26 21:43:09 +0300

    Initial attempt at using YBC

--- pogo-browser/src/main.rs
+++ pogo-browser/src/main.rs
@@ -1,43 +1,46 @@
+use ybc::NavbarFixed::Top;
+use ybc::TileCtx::{Ancestor, Child, Parent};
+use ybc::TileSize::Three;
 use yew::prelude::*;
-use yew_bootstrap::component::Button;
-use yew_bootstrap::util::{include_inline, Color};
 
-enum Msg {
-    AddOne,
-}
+enum Msg {}
 
-struct Model {
-    value: i64,
-}
+struct Model {}
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
+        Self {}
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+        match msg {}
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         let link = ctx.link();
+        let brand = html! {<h1 style="font-size: 20pt">{"Pogo Browser"}</h1> };
         html! {
-            <div>
-                {include_inline()}
-                <Button style={Color::Primary} onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</Button>
-                <p>{ self.value }</p>
-            </div>
+          <>
+          <ybc::Navbar navbrand={brand}>
+            // <ybc::NavbarItem href={"/test"}>
+            //     {"Test link"}
+            // </ybc::NavbarItem>
+          </ybc::Navbar>
+          <ybc::Container fluid={true}>
+            <ybc::Tile ctx={Ancestor}>
+              <ybc::Tile ctx={Parent} vertical={true} size={Three}>
+                {"Item 1"}
+              </ybc::Tile>
+              <ybc::Tile ctx={Parent} vertical={true} size={Three}>
+                {"Item 2"}
+              </ybc::Tile>
+            </ybc::Tile>
+          </ybc::Container>
+          </>
         }
     }
 }

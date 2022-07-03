8f3b26c9427ff16b724757068eb5a77a8dab69c8
Marti Raudsepp <marti@juffo.org>
2022-07-03 13:38:02 +0300

    Show monster base stats

--- pogo-browser/src/monster.rs
+++ pogo-browser/src/monster.rs
@@ -1,6 +1,6 @@
 use crate::common::pretty_name;
 use crate::types::TypePair;
-use pogo_data::schema::PokemonSettings;
+use pogo_data::schema::{MonsterBaseStats, PokemonSettings};
 use std::borrow::Borrow;
 use std::sync::Arc;
 use yew::prelude::*;
@@ -19,12 +19,21 @@ impl PartialEq for MonsterProps {
 #[function_component(Monster)]
 pub fn render_monster(props: &MonsterProps) -> Html {
     let mon: &PokemonSettings = props.mon.borrow();
+
+    let stats = match mon.stats {
+        MonsterBaseStats::Some { base_attack, base_defense, base_stamina } => {
+            html! {<div>{format!("ATK: {base_attack}, DEF: {base_defense}, HP: {base_stamina}")}</div>}
+        }
+        _ => html! {},
+    };
+
     html! {
         <div class="box">
             <p class="subtitle">
                 { pretty_name(&mon.pokemon_id) }
                 <TypePair typ1={mon.type1} typ2={mon.type2} />
             </p>
+            { stats }
         </div>
     }
 }
